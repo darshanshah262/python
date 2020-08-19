5345fd3d1fe6440590b3a9ffcce0b517d11a6f42
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2020-08-19 14:02:10 +0530

    Create function_eg.py

---
+++ function_eg.py
@@ -0,0 +1,13 @@
+def computepay(h,r):
+    if h <= 40 :
+        p = h * r
+    else :
+        p = (h-40) * r * 1.5 + 40 * r
+    return p
+
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate = input('Enter the rate per hour')
+r = float(rate)
+p = computepay(h,r)
+print("Pay",p)
