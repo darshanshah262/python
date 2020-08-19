82330c1d0324678eea23f6c0d0a2605ccc00042b
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2020-08-19 15:58:57 +0530

    Update mail_id.py

--- mail_id.py
+++ mail_id.py
@@ -16,3 +16,25 @@ for lines in fh :
         count = count +1
 
 print("There were", count, "lines in the file with From as the first word")
+
+
+#code for most no.of a id has repeated
+'''
+name = input("Enter file:")
+if len(name) < 1 : 
+    name = "mbox-short.txt"
+handle = open(name)
+wrd = dict()
+for lines in handle:
+    word = lines.split()
+    if 'From:' in word :
+        w = word[1]
+        wrd[w] = wrd.get(w,0) + 1
+largest = -1
+wd = None
+for k,v in wrd.items() :
+    if v > largest :
+        largest = v
+        wd = k
+print(wd,largest)
+'''
