ef593be3f042db9075a1fe06dfa09d3e5feea36e
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2020-08-21 11:16:30 +0530

    Create second_largest.py

---
+++ second_largest.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+
+win = max(arr)
+
+while max(arr) == win:
+    arr.remove(max(arr))
+
+print(max(arr))
+
+
+''' 
+under logic part this can also be used:
+
+for i in arr:
