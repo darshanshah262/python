20e91dacaf48603836e7562c7b97068f31ca2486
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2020-08-27 18:38:36 +0530

    Create number_system.py

---
+++ number_system.py
@@ -0,0 +1,18 @@
+'''
+input - n(number)
+output - decimail,octal,hexa,bin values from 1 to  n
+
+
+
+def print_formatted(number):
+    w = len(str(bin(number)).replace('0b',''))
+    for i in range(1, number+1):
+        b = bin(int(i)).replace('0b','').rjust(w, ' ')
+        o = oct(int(i)).replace('0o','').rjust(w, ' ')
+        h = hex(int(i)).replace('0x','').upper().rjust(w, ' ')
+        j = str(i).rjust(w, ' ')
+        print (j, o, h, b)
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
