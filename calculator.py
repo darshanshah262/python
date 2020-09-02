530d8bfc36a27340e20d10624183050ce2b04696
darshanshah262 <48462723+darshanshah262@users.noreply.github.com>
2020-09-02 16:24:21 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -33,6 +33,9 @@ def calculate():
 # Function which takes operator as input and displays it on the input field
 def get_operation(operator):
     global i
+    entire_string = display.get()
+    if entire_string == 'Error':
+        clear_all()
     length = len(operator)
     display.insert(i,operator)
     i+=length
@@ -69,45 +72,45 @@ def fact():
 
 #adding the input field
 display = Entry(root)
-display.grid(row=1,columnspan=6,sticky=N+E+W+S)
+display.grid(row=1,columnspan=6,sticky=N+E+W+S,ipadx = 15, ipady = 15)
 
 #Code to add buttons to the Calculator
 
-Button(root,text="1",command = lambda :get_variables(1)).grid(row=2,column=0, sticky=N+S+E+W)
-Button(root,text=" 2",command = lambda :get_variables(2)).grid(row=2,column=1, sticky=N+S+E+W)
-Button(root,text=" 3",command = lambda :get_variables(3)).grid(row=2,column=2, sticky=N+S+E+W)
+Button(root,text="1",command = lambda :get_variables(1)).grid(row=2,column=0, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text=" 2",command = lambda :get_variables(2)).grid(row=2,column=1, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text=" 3",command = lambda :get_variables(3)).grid(row=2,column=2, sticky=N+S+E+W,ipadx = 15, ipady = 15)
 
-Button(root,text="4",command = lambda :get_variables(4)).grid(row=3,column=0, sticky=N+S+E+W)
-Button(root,text=" 5",command = lambda :get_variables(5)).grid(row=3,column=1, sticky=N+S+E+W)
-Button(root,text=" 6",command = lambda :get_variables(6)).grid(row=3,column=2, sticky=N+S+E+W)
+Button(root,text="4",command = lambda :get_variables(4)).grid(row=3,column=0, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text=" 5",command = lambda :get_variables(5)).grid(row=3,column=1, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text=" 6",command = lambda :get_variables(6)).grid(row=3,column=2, sticky=N+S+E+W,ipadx = 15, ipady = 15)
 
-Button(root,text="7",command = lambda :get_variables(7)).grid(row=4,column=0, sticky=N+S+E+W)
-Button(root,text=" 8",command = lambda :get_variables(8)).grid(row=4,column=1, sticky=N+S+E+W)
-Button(root,text=" 9",command = lambda :get_variables(9)).grid(row=4,column=2, sticky=N+S+E+W)
+Button(root,text="7",command = lambda :get_variables(7)).grid(row=4,column=0, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text=" 8",command = lambda :get_variables(8)).grid(row=4,column=1, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text=" 9",command = lambda :get_variables(9)).grid(row=4,column=2, sticky=N+S+E+W,ipadx = 15, ipady = 15)
 
 #adding other buttons to the calculator
-Button(root,text="AC",command=lambda :clear_all()).grid(row=5,column=0, sticky=N+S+E+W)
-Button(root,text=" 0",command = lambda :get_variables(0)).grid(row=5,column=1, sticky=N+S+E+W)
-Button(root,text=" .",command=lambda :get_variables(".")).grid(row=5, column=2, sticky=N+S+E+W)
+Button(root,text="AC",command=lambda :clear_all()).grid(row=5,column=0, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text=" 0",command = lambda :get_variables(0)).grid(row=5,column=1, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text=" .",command=lambda :get_variables(".")).grid(row=5, column=2, sticky=N+S+E+W,ipadx = 15, ipady = 15)
 
 
-Button(root,text="+",command= lambda :get_operation("+")).grid(row=2,column=3, sticky=N+S+E+W)
-Button(root,text="-",command= lambda :get_operation("-")).grid(row=3,column=3, sticky=N+S+E+W)
-Button(root,text="*",command= lambda :get_operation("*")).grid(row=4,column=3, sticky=N+S+E+W)
-Button(root,text="/",command= lambda :get_operation("/")).grid(row=5,column=3, sticky=N+S+E+W)
+Button(root,text="+",command= lambda :get_operation("+")).grid(row=2,column=3, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text="-",command= lambda :get_operation("-")).grid(row=3,column=3, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text="*",command= lambda :get_operation("*")).grid(row=4,column=3, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text="/",command= lambda :get_operation("/")).grid(row=5,column=3, sticky=N+S+E+W,ipadx = 15, ipady = 15)
 
 # adding new operations
-Button(root,text="pi",command= lambda :get_operation("*3.14")).grid(row=2,column=4, sticky=N+S+E+W)
-Button(root,text="%",command= lambda :get_operation("%")).grid(row=3,column=4, sticky=N+S+E+W)
-Button(root,text="(",command= lambda :get_operation("(")).grid(row=4,column=4, sticky=N+S+E+W)
-Button(root,text="exp",command= lambda :get_operation("**")).grid(row=5,column=4, sticky=N+S+E+W)
+Button(root,text="pi",command= lambda :get_operation("*3.14")).grid(row=2,column=4, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text="%",command= lambda :get_operation("%")).grid(row=3,column=4, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text="(",command= lambda :get_operation("(")).grid(row=4,column=4, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text="exp",command= lambda :get_operation("**")).grid(row=5,column=4, sticky=N+S+E+W,ipadx = 15, ipady = 15)
 
-Button(root,text="<-",command= lambda :undo()).grid(row=2,column=5, sticky=N+S+E+W)
-Button(root,text="x!", command= lambda: fact()).grid(row=3,column=5, sticky=N+S+E+W)
-Button(root,text=")",command= lambda :get_operation(")")).grid(row=4,column=5, sticky=N+S+E+W)
-Button(root,text="^2",command= lambda :get_operation("**2")).grid(row=5,column=5, sticky=N+S+E+W)
-Button(root,text="^2",command= lambda :get_operation("**2")).grid(row=5,column=5, sticky=N+S+E+W)
-Button(root,text="=",command= lambda :calculate()).grid(columnspan=6, sticky=N+S+E+W)
+Button(root,text="<-",command= lambda :undo()).grid(row=2,column=5, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text="x!", command= lambda: fact()).grid(row=3,column=5, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text=")",command= lambda :get_operation(")")).grid(row=4,column=5, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text="^2",command= lambda :get_operation("**2")).grid(row=5,column=5, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text="^2",command= lambda :get_operation("**2")).grid(row=5,column=5, sticky=N+S+E+W,ipadx = 15, ipady = 15)
+Button(root,text="=",command= lambda :calculate()).grid(columnspan=6, sticky=N+S+E+W,ipadx = 15, ipady = 15)
 
 
 root.mainloop()
