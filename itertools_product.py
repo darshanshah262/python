168c88fb055e8285345b852dc1dc58ec66e94256
darshanshah262 <darshanshah262@gmail.com>
2021-04-29 23:16:51 +0530

    itertools_product

---
+++ itertools_product.py
@@ -0,0 +1,15 @@
+'''
+A = [1, 2]
+B = [3, 4]
+
+AxB = [(1, 3), (1, 4), (2, 3), (2, 4)]
+'''
+
+
+
+from itertools import product
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+print(*product(a, b))
