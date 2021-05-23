79a37aebfc7900d7e3be1e9866cac8d7df58c5fb
darshanshah262 <darshanshah262@gmail.com>
2021-05-23 13:06:09 +0530

    corelation

---
+++ corelation.py
@@ -0,0 +1,16 @@
+def relation(n,k):
+    l=[]
+    a =[]
+    c=0
+    for i in range(n):
+        l.append(int(input()))
+    for i in range(len(l)):
+        if (n-l[i]) % k == 0:
+            c+=1
+            a.append(l[i])
+    print(c)
+    for i in a:
+        print(i)
+
+n,k = map(int,input().split())
+relation(n,k)
